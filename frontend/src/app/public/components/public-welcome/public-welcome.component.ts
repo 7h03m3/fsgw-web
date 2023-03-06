3df1d925af88dadfce5f23fd852c0bd1e9872297
Thomas Stanger <stangth1@students.zhaw.ch>
2023-03-06 20:25:11 +0100

    feat(frontend): add welcome page

--- frontend/src/app/public/components/public-welcome/public-welcome.component.ts
+++ frontend/src/app/public/components/public-welcome/public-welcome.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-public-welcome',
   templateUrl: './public-welcome.component.html',
-  styleUrls: ['./public-welcome.component.css']
+  styleUrls: ['./public-welcome.component.css'],
 })
 export class PublicWelcomeComponent {
+  constructor(private router: Router) {}
 
+  public onEventSelect(category: string | undefined) {
+    if (category) {
+      this.router.navigate(['/public/event-list', { category: category }]);
+    } else {
+      this.router.navigate(['/public/event-list']);
+    }
+  }
 }
